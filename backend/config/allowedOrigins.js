76e8f8690ff6b53a7c0afbd6882e4a2abf547ee3
mengzhuou <mou30@gatech.edu>
2024-11-22 18:21:01 -0500

    update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://lmcfilms.netlify.app',
+    'https://lmcfilms-backend-ad47e3b57e1a.herokuapp.com/'
     // 'https://www.lmcfilms.lmc.gatech.edu/',
     // 'https://lmcfilms.lmc.gatech.edu/'
 ]
